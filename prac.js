3dafb954a2fcf4152debeef2d5cb463927ad0316
Mufeez <mziix007@gmail.com>
2024-01-23 16:49:42 +0530

    weird

--- prac.js
+++ prac.js
@@ -1,7 +1,23 @@
-var a = 3;
-var b = 8;
-a = a+ b;
-b = a - b;
-a = a - b;
-console.log(a)
-console.log(b)  
+// var a = 3;
+// var b = 8;
+// a = a+ b;
+// b = a - b;
+// a = a - b;
+// console.log(a)
+// console.log(b)  
+// var myName= "Mzi"
+// console.log(myName)
+// var tweet =prompt("enter your message ");
+// var l = tweet.length;
+// var s = 150;
+// var r = l-s;
+// alert("you  have written" +l+ "characters, you have" +r+ "left ");
+// var name = prompt("your name ");
+// alert("Hello"+name.slice(0,1).toUpperCase+name.slice(1,).tolowercase);
+
+// var name = prompt("what is your name ");
+
+// var dogAge = prompt("enter dog age");
+// var humanAge = ((dogAge-2)*4)+21;
+// alert(humanAge)
+
